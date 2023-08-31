f56193bcd91a84b21d78230e6de491411b66a800
mahmoud-ali000 <127243561+mahmoud-ali000@users.noreply.github.com>
2023-08-31 06:26:25 +0300

    initial commit

---
+++ lib/stacks.py
@@ -0,0 +1,18 @@
+def is_balanced(expression):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+    
+    for char in expression:
+        if char in '([{':
+            stack.append(char)
+        elif char in ')]}':
+            if not stack or stack[-1] != mapping[char]:
+                return False
+            stack.pop()
+    
+    return len(stack) == 0
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))
+print(is_balanced(expression2))  
